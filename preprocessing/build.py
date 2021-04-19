7d6ba411dea289899b1fc373049cf97ea033cfbc
ungzs1 <uuungvarszi@gmail.com>
2021-04-19 14:07:07 +0200

    changes in the preprocessing functions

--- preprocessing/build.py
+++ preprocessing/build.py
@@ -13,9 +13,13 @@ try:
 except ImportError:
     from preprocessor_HTNet_data import *
 
-### UNCOMMENT THE PROPER LINE TO BULD THE PREPROCESSED DATABASE
-
+# ** USE THIS PYTHON FILE TO EASILY BUILD PREPROCESSED DATASETS WITH DIFFERENT PARAMETERS **
 #StanfordPreprocessor().run()
 #Bci3Preprocessor().run()
-HTNet_preprocessor().run()
+
+time_ranges = [(500, -1), (500, -100), (500, -200), (500, -300), (500, -400)]
+for time_range in time_ranges:
+    HtnetPreprocessor().__init__()
+    HtnetPreprocessor().time_range = time_range
+    HtnetPreprocessor().run()
 
--- preprocessing/preprocessor_base.py
+++ preprocessing/preprocessor_base.py
@@ -8,7 +8,8 @@ import h5py
 
 from scipy.signal import butter, lfilter, iirnotch, filtfilt, welch
 from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+
 
 def butter_bandpass(lowcut, highcut, fs, order=5, btype="band"):
     nyq = 0.5 * fs
@@ -17,60 +18,70 @@ def butter_bandpass(lowcut, highcut, fs, order=5, btype="band"):
     b, a = butter(order, [low, high], btype=btype)
     return b, a
 
+
 def butter_bandpass_filter(data, lowcut, highcut, fs, order=5, axis=0, btype="band"):
     b, a = butter_bandpass(lowcut, highcut, fs, order=order, btype=btype)
     y = lfilter(b, a, data, axis=axis)
     return y
 
-def notch_filtering(data, fs, line_freq, PSD_range):
+
+def notch_filtering(data, fs, line_freq, psd_range):
     # removes line frequency noise and multiples from the signal
 
-    multiples_linefreq = range(line_freq,PSD_range[1],line_freq) # line frequency and multiples eg. [60,120,180]
+    multiples_linefreq = range(line_freq, psd_range[1], line_freq)  # line frequency and multiples eg. [60,120,180]
 
-    for f0 in multiples_linefreq:    
-        w0 = f0 / (fs / 2 ) # Normalized Frequency
+    for f0 in multiples_linefreq:
+        w0 = f0 / (fs / 2)  # Normalized Frequency
         b, a = iirnotch(w0, fs)  # Numerator ('b') and denominator ('a') polynomials of the IIR filter
         data = filtfilt(b, a, data, axis=2)
-        
+
     return data
+
 
 def car(data):
     # this function calculates and returns the common avg reference of 2-d matrix "data" of shape [timestep, channel].
-    data = np.double(data)        
+    data = np.double(data)
     num_chans = data.shape[1]
-    
+
     # create a CAR spatial filter
-    spatfiltmatrix = -np.ones((num_chans,num_chans))
+    spatfiltmatrix = -np.ones((num_chans, num_chans))
     for i in range(num_chans):
-        spatfiltmatrix[i, i] = num_chans-1
-    spatfiltmatrix = spatfiltmatrix/num_chans
+        spatfiltmatrix[i, i] = num_chans - 1
+    spatfiltmatrix = spatfiltmatrix / num_chans
 
     # perform spatial filtering
     for i in range(data.shape[0]): data[i] = np.dot(spatfiltmatrix, data[i])
 
     return data
 
-def get_spectra(x, fs, time_range, freq_range):
-    # returns PSD as "all_PSD" in form of [frequencies, channels, trials]. power spectrum densitiy for each channel for each trial
-    noverlap = np.floor(fs*0.1); nperseg = np.floor(fs*0.25) # set window size and offset for PSD
-    all_PSD = []
 
-    for curr_data in x: # loop through all trials
-        block_PSD = []
+def get_spectra(x, fs, freq_range):
+    """
+    returns PSD as "all_psd" in form of [frequencies, channels, trials]. power spectrum densitiy for each channel
+    for each trial
+    """
+
+    noverlap = np.floor(fs * 0.1);
+    nperseg = np.floor(fs * 0.25)  # set window size and offset for PSD
+    all_psd = []  # PSD for all trials and channels
+
+    for curr_data in x:  # loop through all trials
+        block_psd = []  # PSD for features in current 'block' ie all channels for this trial
         # get Power Spectrum Density with signal.welch
-        for ch in range(x.shape[1]): # loop through all channels
-            [f, temp_PSD] = welch(curr_data[ch,:], nfft=fs, fs=fs, noverlap=noverlap, nperseg=nperseg)
-            block_PSD.append(temp_PSD)
+        for ch in range(x.shape[1]):  # loop through all channels
+            [f, temp_psd] = welch(curr_data[ch, :], nfft=fs, fs=fs, noverlap=noverlap, nperseg=nperseg)
+            temp_psd = temp_psd[np.where((freq_range[0] <= f) & (f <= freq_range[1]))]  # to get the desired freq range only
+            block_psd.append(temp_psd)
 
-        block_PSD = np.asarray(block_PSD)
-        all_PSD.append(block_PSD[:,0:200]) # downsample - we only want to get spectra of PSD_range
+        block_psd = np.asarray(block_psd)
+        all_psd.append(block_psd)
 
-    all_PSD = np.asarray(all_PSD)
+    all_psd = np.asarray(all_psd)
 
-    return all_PSD
+    return all_psd
+
 
 def config_post(path, key, value):
-        
     if key.endswith("value"):
         try:
             return key, int(value)
@@ -85,17 +96,20 @@ def config_post(path, key, value):
 
     return key, value
 
+
 class Preprocessor(object):
+    fs = -1  # sampling rate
+    line_freq = -1  # line freq = 60 Hz
+    blocksize = np.floor(fs * 0.25)  # minimum block size of trials, shorter trials ignored.
+    # Default: blocksize=floor(fs*0.25) window length of PSD
+    time_range = (0, -1)  # set time range of trials to use in a tuple of (first data, last data),
+    # eg (1000,-500) ignores first 1000 and last 500 data points. Default: PSD_time_range=(0,-1) to use whole range
+    freq_range = (0, 200)  # range of Power Spectrum, min and max freq in a tuple eg.(0,200)
+    # gives power spectrum from 0 to 199 Hz. Default: PSD_freq_range=(0,200)
+    subject_ids = []  # store subject ids to ease their access
 
-    fs = -1 # sampling rate
-    line_freq = -1 # line freq = 60 Hz
-    blocksize = np.floor(fs*0.25) # minimum block size of trials, shorter trials ignored. Default: blocksize=floor(fs*0.25) window length of PSD
-    PSD_time_range = (0,-1) # set time range of trials to use in a tuple of (first data, last data), eg (1000,-500) ignores first 1000 and last 500 datapoints. Default: PSD_time_range=(0,-1) to use whole range
-    PSD_freq_range = (0,200) # range of Power Spectrum, min and max freq in a tuple eg.(0,200) gives power spectrum from 0 to 199 Hz. Default: PSD_freq_range=(0,200)
-
-    def __init__(self, config_file ="", config={}):
-
-        if config_file != "" and path.exists(config_file):
+    def __init__(self, config_file="", config={}):
+        if config_file != "" and os.path.exists(config_file):
             with open(config_file) as fd:
                 try:
                     self.config = xmltodict.parse(fd.read(), dict_constructor=dict, postprocessor=config_post)
@@ -106,9 +120,8 @@ class Preprocessor(object):
                     self.config = config
         else:
             self.config = config
-
         self.config.setdefault("save_dir", "")
-        self.config.setdefault("save_name","") 
+        self.config.setdefault("save_name", "")
         self.config.setdefault("default_config_name", "config.xml")
         self.config.setdefault("create_validation_bool", True)
         self.config.setdefault("data_source", "")
@@ -117,10 +130,13 @@ class Preprocessor(object):
         # should return a pair of numpy arrays of dimensions ( [timestep, channels], [timestep, label] )
         raise NotImplementedError
 
+    def get_train_test_data(self, *args):
+        raise NotImplementedError
+
     def train_files_from_dir(self):
         # return all the valid train files in a list
         raise NotImplementedError
-    
+
     def test_files_from_dir(self):
         # return all the valid test files in a list
         raise NotImplementedError
@@ -131,26 +147,27 @@ class Preprocessor(object):
         test_x = []
         test_y = []
 
-        print('reading data and reshaping it...')
-        X_train_all, y_train_all, X_test_all, y_test_all = self.get_train_test_data()
+        print('reading and preprocessing data...')
+        for i, subject in enumerate(Preprocessor.subject_ids):
+            print('\t{}/{}\n\tpatient id: {}'.format(i+1, len(Preprocessor.subject_ids), subject))
 
-        print('preprocessing train data...')
-        for i in range(len(X_train_all)):
-            print('\tsubject ', i, '/', len(X_train_all))
-            x = X_train_all[i]
-            y = y_train_all[i]
-            px,py = self.preprocess_with_label(x,y)
+            # get train and test data for current subject
+            X_train, y_train, X_test, y_test = self.get_train_test_data(subject)
+
+            # preprocess train data
+            x = X_train
+            y = y_train
+            px, py = self.preprocess_with_label(x, y)
             train_x.append(px)
             train_y.append(py)
 
-        print('\npreprocessing test data...')
-        for i in range(len(X_test_all)):
-            print('\tsubject ', i, '/', len(X_train_all))
-            x = X_test_all[i]
-            y = y_test_all[i]       
-            px,py = self.preprocess_with_label(x,y)
+            # preprocess test data
+            x = X_test
+            y = y_test
+            px, py = self.preprocess_with_label(x, y)
             test_x.append(px)
             test_y.append(py)
+
         '''
         for ftrain in self.train_files_from_dir():
             print(ftrain) # to see progress
@@ -166,16 +183,16 @@ class Preprocessor(object):
             test_y.append(py)
         '''
         # Sanity check
-        
+
         if len(train_x) != len(train_y):
             raise ValueError("Train dataset first dimension mismatch: ", len(train_x), " and ", len(train_y))
 
         if len(test_x) != len(test_y):
             raise ValueError("Test dataset first dimension mismatch: ", len(train_x), " and ", len(train_y))
-        
+
         if self.config["create_validation_bool"]:
             train_x, train_y, val_x, val_y = self.create_validation_from_train(train_x, train_y)
-        
+
         # Create datasets
         print('saving datasets...')
         with h5py.File(os.path.join(self.config["save_dir"], self.config["save_name"]), 'w') as hf:
@@ -190,15 +207,15 @@ class Preprocessor(object):
             for i, py in enumerate(test_y): grp_test_y[self.subject_ids[i]] = py
 
             if self.config["create_validation_bool"]:
-                hf.create_dataset("val_x",  data=val_x)
-                hf.create_dataset("val_y",  data=val_y)
+                hf.create_dataset("val_x", data=val_x)
+                hf.create_dataset("val_y", data=val_y)
 
         if not os.path.exists(self.config["save_dir"]):
             os.makedirs(self.config["save_dir"])
 
         with open(os.path.join(self.config["save_dir"], self.config["default_config_name"]), "w") as fd:
-            root = {"root":self.config}
-            fd.write(xmltodict.unparse(root, pretty = True))
+            root = {"root": self.config}
+            fd.write(xmltodict.unparse(root, pretty=True))
 
         print("Preprocessing done")
 
@@ -219,25 +236,27 @@ class Preprocessor(object):
         px = []
         py = []
 
-
         ## ACTUAL PREPROCESSING ##
 
-        # Notch filtering to remove line frequency noise and multiples in PSD_range
-        x = notch_filtering(x, self.fs, self.line_freq, self.PSD_freq_range)
+        # Notch filtering to remove line frequency noise and its multiplicands, in the desired frequency range (note
+        # that frequencies outside of this range will be excluded from the data anyways
+        x = notch_filtering(x, self.fs, self.line_freq, self.freq_range)
 
         # Common Average Reference (car) filter to remove noise common to all channels
         x = car(x)
 
-        # Calculate spectra from 0 to 200 Hz
-        px = get_spectra(x, self.fs, self.PSD_time_range, self.PSD_freq_range)
+        # extract desired time range
+        t_start = self.time_range[0]
+        t_end = self.time_range[1]
+        x = x[:, :, t_start:t_end]
 
-        py = y # nothing to change
+        # Calculate spectra from 0 to 200 Hz
+        px = get_spectra(x, self.fs, self.freq_range)
+        py = y  # nothing to change
 
         ## 
 
         px = np.asarray(px)
         py = np.asarray(py)
 
-        return px,py
-
-
+        return px, py
